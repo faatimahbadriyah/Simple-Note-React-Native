18e2d9b315660bc16c981331cd05c761ed095afe
fatimah.badriyah <fbadriyah07@gmail.com>
2019-01-17 16:28:45 +0700

    First Commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Main from './app/components/Main';
+import {
+	Stylesheet,
+	Text,
+	TextInput,
+	View,
+	ScrollView,
+	TouchableOpacity,
+} from 'react-native';
+class App extends React.Component {
+	render(){
+		return(
+			<Text>Try</Text>
+		);
+	}
+}
+
+export default App;
